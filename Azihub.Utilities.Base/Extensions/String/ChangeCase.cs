c36b6f9ba5dc4181730e7f3252eddb2416dc2116
Mason Chase <17309021+mason-chase@users.noreply.github.com>
2021-07-11 10:07:07 +0200

    Added ChangeCase

---
+++ Azihub.Utilities.Base/Extensions/String/ChangeCase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Azihub.Utilities.Base.Extensions.String
+{
+    public static class StringUtilities
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            var result = string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+            return result;
+        }
+
+        public static string ToPascalCase(this string original)
+        {
+            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
+            Regex whiteSpace = new Regex(@"(?<=\s)");
+            Regex startsWithLowerCaseChar = new Regex("^[a-z]");
+            Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$");
+            Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]");
+            Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))");
+
+            // replace white spaces with undescore, then replace all invalid chars with empty string
+            var pascalCase = invalidCharsRgx.Replace(whiteSpace.Replace(original, "_"), string.Empty)
+                // split by underscores
+                .Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
+                // set first letter to uppercase
+                .Select(w => startsWithLowerCaseChar.Replace(w, m => m.Value.ToUpper()))
+                // replace second and all following upper case letters to lower if there is no next lower (ABC -> Abc)
+                .Select(w => firstCharFollowedByUpperCasesOnly.Replace(w, m => m.Value.ToLower()))
+                // set upper case the first lower case following a number (Ab9cd -> Ab9Cd)
+                .Select(w => lowerCaseNextToNumber.Replace(w, m => m.Value.ToUpper()))
+                // lower second and next upper case letters except the last if it follows by any lower (ABcDEf -> AbcDef)
+                .Select(w => upperCaseInside.Replace(w, m => m.Value.ToLower()));
+
+            return string.Concat(pascalCase);
+        }
+
+        public static string ToConstantCase(this string original, bool addPrefix = false)
+        {
+            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
+            Regex whiteSpace = new Regex(@"(?<=\s)");
+
+            // replace white spaces with undescore, then replace all invalid chars with empty string
+            var constantCase = invalidCharsRgx.Replace(whiteSpace.Replace(original, "_"), string.Empty)
+                // split by underscores
+                .Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
+                // set first letter to uppercase
+                .Select(w => w.ToUpper());
+
+
+            string joined = string.Join("_", constantCase);
+            if ((addPrefix) && Regex.IsMatch(joined, @"^\d"))
+                joined = "_" + joined;
+
+            return joined;
+        }
+
+        public static string EscapeQuotes(this string original)
+        {
+            return original.Replace(@"""", @"""""");
+        }
+
+        public static string ToCodeString(this Uri original)
+        {
+            if (original is null)
+                return "null";
+
+            return string.IsNullOrEmpty(original.ToString()) ? "null" : $@"new Uri(""{original}"")";
+        }
+    }
+}
